9a3710d6a185ce4ff79def776a0b4af51ecc55e7
Manoj Bhadane <manojbhadane777@gmail.com>
2022-06-28 10:22:27 +0530

    Logger added

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,16 +14,20 @@ import java.util.Optional;
 @RestController
 public class DepartmentController {
 
+    public final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @Autowired
     private DepartmentService departmentService;
 
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside save department");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> getDepartments(){
+        LOGGER.info("Inside get department");
         return departmentService.getDepartments();
     }
 
@@ -32,6 +38,7 @@ public class DepartmentController {
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartment(@PathVariable("id") Long departmentId){
+        LOGGER.info("Inside delete department");
         departmentService.deleteDepartment(departmentId);
         return "Department deleted successfully";
     }
